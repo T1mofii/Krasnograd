a9900816513a2c5ba29fb458edab54d9ee5a479c
Timofii <108995136+T1mofii@users.noreply.github.com>
2024-02-10 18:52:51 +0100

    Update form.js

--- form.js
+++ form.js
@@ -1,1 +1,20 @@
+function submitForm(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение отправки формы
+
+    const formData = new FormData(event.target); // Получаем данные формы
+    const formUrl = 'https://app.netlify.com/sites/krasnograd/forms'; // Замените на ваш URL формы Netlify
+
+    fetch(formUrl, {
+        method: 'POST',
+        body: formData // Отправляем данные на URL формы Netlify методом POST
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        alert('Дякуємо за заповнення анкети'); // Оповещение об успешной отправке
+        event.target.reset(); // Сброс формы после успешной отправки
+    })
+    .catch(error => console.error('There has been a problem with your fetch operation:', error));
+}
 
